1cfed5c4fa563678ed8432671e1cdc18ebfea407
F3n1X <54713542+LightF3n1X@users.noreply.github.com>
2022-02-04 19:04:26 +0200

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,45 @@
-package domain;
-
-public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+public  class Exercise{
+  public static String toBinary(int decimal){
+      int binary[] = new int[40];
+      int index = 0;
+      String binary_string = "";
+      while(decimal > 0){
+        binary[index++] = decimal%2;
+        decimal = decimal/2;
+      }
+      for(int i = index-1;i >= 0;i--){
+        binary_string += binary[i];
+      }
+      return binary_string;
+  }
+  
+  public static String toOctal(int decimal){
+    int rem;
+    String octal="";
+    char octalchars[]={'0','1','2','3','4','5','6','7'};
+    while(decimal>0)
+    {
+       rem=decimal%8;
+       octal=octalchars[rem]+octal;
+       decimal=decimal/8;
+    }
+    return octal;
+  }
+  
+  public static String toHex(int decimal){    
+     int rem;  
+     String hex="";   
+     char hexchars[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};  
+    while(decimal>0)  
+     {  
+       rem=decimal%16;   
+       hex=hexchars[rem]+hex;   
+       decimal=decimal/16;  
+     }  
+    return hex;  
+  }    
+  
+   public static String[] Calculate(int decimal) {
+      return new String[] {String.valueOf(toBinary(decimal)), String.valueOf(toOctal(decimal)), String.valueOf(toHex(decimal))};
     }
 }
